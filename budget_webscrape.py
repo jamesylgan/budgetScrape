956a66f8379c282c120b9aba3b9563fbf03f400e
James Gan <jg893@cornell.edu>
2017-07-07 13:03:34 -0400

    cleaned up a bit

--- budget_webscrape.py
+++ budget_webscrape.py
@@ -11,10 +11,7 @@ from bs4 import BeautifulSoup
 
 
 """ TO-DO:
- 1. Add more years
- 2. Add all the ATS codes
- 3. Add better keywords
- 4. Add a better seperator
+ 3. School Name
  5. Add in better specification
  6. Make quicker
  
@@ -45,18 +42,21 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
  """
 
-codes = []
+codes = ["14K614", "19K662", "08X301", "09X042", "09X132", "18K272", "03M299",
+		"09X236", "18K211", "18K366", "84R073", "09X311", "23K446", "23K668", "12X129", 
+		"09X117", "11X103", "28Q050", "27Q319", "27Q042", "27Q062", "16K005", "27Q053", 
+		"12X196", "07X001", "28Q008", "08X269", "09X218", "23K284", "27Q105", "27Q105",
+		"04M050", "28Q217"]
 
 keywords = ["CONSULTANTS", "PROFESSIONAL", "ABSENCE", "SUMMER",
-		"DEVELOPMENT", "SUPPORT STAFF", 
-		"SERVICES", "PROGRAMS"]
+			"DEVELOPMENT", "SUPPORT STAFF", "SERVICES", "PROGRAMS"]
 
-def budgetFetch17(ATS):
+def budgetFetch(ATS, year):
 
 	# figure out URL format
 	url = "https://www.nycenet.edu/offices/d_chanc_oper/budget/dbor/galaxy/"+\
-	"galaxybudgetsummaryto/display2.asp?DDBSSS_INPUT="+ATS+\
-	"&Submit=Enter&PROG_YEAR=2017&POP_SCH="+ATS
+	"galaxybudgetsummaryto/display2.asp?DDBSSS_INPUT="+ATS[2:]+\
+	"&Submit=Enter&PROG_YEAR="+year+"&POP_SCH="+ATS[2:]
 
 	page = requests.get(url)
 
@@ -98,28 +98,17 @@ def budgetFetch17(ATS):
 		for term in keywords:
 			if (((term) in line) and not printed):
 				printed = True
-				print(line)
+				#print(line)
 				with open("Budgets.txt", "a") as text_file:
 					text_file.write(line+'\n')
+	
+	linesTotal = []
 
-def budgetFetch16(ATS):
+	printedP = False
 
-	# figure out URL format
-	url = "https://www.nycenet.edu/offices/d_chanc_oper/budget/dbor/galaxy/"+\
-	"galaxybudgetsummaryto/display2.asp?DDBSSS_INPUT="+ATS+\
-	"&Submit=Enter&PROG_YEAR=2016&POP_SCH="+ATS
-
-	page = requests.get(url)
-
-	if(page.status_code!=200):
-		return "ERROR"
-
-	soup = BeautifulSoup(page.content, 'html.parser')
-
-	lines = []
-
+	# Finding total and principal
 	# Better looking output: nothing colliding
-	for line in soup.find_all('tr', class_='mysection'):
+	for line in soup.find_all('tr', class_='myschool'):
 		lineStr = line.get_text()
 		if len(lineStr) > 0:
 			for i in range(0, len(lineStr)-1):
@@ -127,150 +116,44 @@ def budgetFetch16(ATS):
 				lineStr[i].isalpha() and lineStr[i+1].isalpha():
 					lineStr = lineStr[:i+1] + ' ' + lineStr[i+1:]
 
-		# lame hardcode to remove irrelevant data
-		lineStr = lineStr.replace("0.00", "")
-		lineStr = lineStr.replace("1.00", "")
-		lineStr = lineStr.replace("2.00", "")
-		lineStr = lineStr.replace("3.00", "")
-		lineStr = lineStr.replace("4.00", "")
-		lineStr = lineStr.replace("5.00", "")
-		lineStr = lineStr.replace("6.00", "")
-		lineStr = lineStr.replace("7.00", "")
-		lineStr = lineStr.replace("8.00", "")
-		lineStr = lineStr.replace("9.00", "")
-		lineStr = lineStr.replace("0.50", "")
+		# lame hardcode
 		lineStr = lineStr.replace("OTPS", "")
 		lineStr = lineStr.replace("SBST", "")
-		lines.append(lineStr)
+		lineStr = lineStr.replace(ATS, "")
 
-	# O(n^2+n), could improve
-	for line in lines:
-		printed = False
-		for term in keywords:
-			if (((term) in line) and not printed):
-				printed = True
-				print(line)
-				with open("Budgets.txt", "a") as text_file:
-					text_file.write(line+'\n')
+		# add total
+		if '$' in lineStr:
+			lineStr = lineStr.replace("$", " $")
+			lineStr = "Positions & Total Budget: $ "+lineStr
+			linesTotal.append(lineStr)
 
-def budgetFetch15(ATS):
+		# add principal
+		if printedP is False:
+			if ((", " in lineStr) and not lineStr[lineStr.find(',')-1].isupper()):
+				lineStr = "Principal: $ "+lineStr
+				linesTotal.append(lineStr.strip())
+				printedP = True
 
-	# figure out URL format
-	url = "https://www.nycenet.edu/offices/d_chanc_oper/budget/dbor/galaxy/"+\
-	"galaxybudgetsummaryto/display2.asp?DDBSSS_INPUT="+ATS+\
-	"&Submit=Enter&PROG_YEAR=2015&POP_SCH="+ATS
-
-	page = requests.get(url)
-
-	if(page.status_code!=200):
-		return "ERROR"
-
-	soup = BeautifulSoup(page.content, 'html.parser')
-
-	lines = []
-
-	# Better looking output: nothing colliding
-	for line in soup.find_all('tr', class_='mysection'):
-		lineStr = line.get_text()
-		if len(lineStr) > 0:
-			for i in range(0, len(lineStr)-1):
-				if not lineStr[i].isupper() and lineStr[i+1].isupper() and \
-				lineStr[i].isalpha() and lineStr[i+1].isalpha():
-					lineStr = lineStr[:i+1] + ' ' + lineStr[i+1:]
-
-		# lame hardcode to remove irrelevant data
-		lineStr = lineStr.replace("0.00", "")
-		lineStr = lineStr.replace("1.00", "")
-		lineStr = lineStr.replace("2.00", "")
-		lineStr = lineStr.replace("3.00", "")
-		lineStr = lineStr.replace("4.00", "")
-		lineStr = lineStr.replace("5.00", "")
-		lineStr = lineStr.replace("6.00", "")
-		lineStr = lineStr.replace("7.00", "")
-		lineStr = lineStr.replace("8.00", "")
-		lineStr = lineStr.replace("9.00", "")
-		lineStr = lineStr.replace("0.50", "")
-		lineStr = lineStr.replace("OTPS", "")
-		lineStr = lineStr.replace("SBST", "")
-		lines.append(lineStr)
-
-	# O(n^2+n), could improve
-	for line in lines:
-		printed = False
-		for term in keywords:
-			if (((term) in line) and not printed):
-				printed = True
-				print(line)
-				with open("Budgets.txt", "a") as text_file:
-					text_file.write(line+'\n')
-
-
-def budgetFetch14(ATS):
-
-	# figure out URL format
-	url = "https://www.nycenet.edu/offices/d_chanc_oper/budget/dbor/galaxy/"+\
-	"galaxybudgetsummaryto/display2.asp?DDBSSS_INPUT="+ATS+\
-	"&Submit=Enter&PROG_YEAR=2014&POP_SCH="+ATS
-
-	page = requests.get(url)
-
-	if(page.status_code!=200):
-		return "ERROR"
-
-	soup = BeautifulSoup(page.content, 'html.parser')
-
-	lines = []
-
-	# Better looking output: nothing colliding
-	for line in soup.find_all('tr', class_='mysection'):
-		lineStr = line.get_text()
-		if len(lineStr) > 0:
-			for i in range(0, len(lineStr)-1):
-				if not lineStr[i].isupper() and lineStr[i+1].isupper() and \
-				lineStr[i].isalpha() and lineStr[i+1].isalpha():
-					lineStr = lineStr[:i+1] + ' ' + lineStr[i+1:]
-
-		# lame hardcode to remove irrelevant data
-		lineStr = lineStr.replace("0.00", "")
-		lineStr = lineStr.replace("1.00", "")
-		lineStr = lineStr.replace("2.00", "")
-		lineStr = lineStr.replace("3.00", "")
-		lineStr = lineStr.replace("4.00", "")
-		lineStr = lineStr.replace("5.00", "")
-		lineStr = lineStr.replace("6.00", "")
-		lineStr = lineStr.replace("7.00", "")
-		lineStr = lineStr.replace("8.00", "")
-		lineStr = lineStr.replace("9.00", "")
-		lineStr = lineStr.replace("0.50", "")
-		lineStr = lineStr.replace("OTPS", "")
-		lineStr = lineStr.replace("SBST", "")
-		lines.append(lineStr)
-
-	# O(n^2+n), could improve
-	for line in lines:
-		printed = False
-		for term in keywords:
-			if (((term) in line) and not printed):
-				printed = True
-				print(line)
-				with open("Budgets.txt", "a") as text_file:
-					text_file.write(line+'\n')
-
+	for line in linesTotal:
+		with open("Budgets.txt", "a") as text_file:
+			text_file.write(line+'\n')
 
 def main():
+	print("Running...")
 	for code in codes:
+		print("Running for: "+code)
 		with open("Budgets.txt", "a") as text_file:
 			text_file.write('\n\n'+code)
 		with open("Budgets.txt", "a") as text_file:
 			text_file.write('\n'+"2017"+'\n\n')	
-		budgetFetch17(code)
+		budgetFetch(code,"2017")
 		with open("Budgets.txt", "a") as text_file:
 			text_file.write('\n'+"2016"+'\n\n')	
-		budgetFetch16(code)
+		budgetFetch(code,"2016")
 		with open("Budgets.txt", "a") as text_file:
 			text_file.write('\n'+"2015"+'\n\n')	
-		budgetFetch15(code)
+		budgetFetch(code,"2015")
 		with open("Budgets.txt", "a") as text_file:
 			text_file.write('\n'+"2014"+'\n\n')	
-		budgetFetch14(code)
-		
+		budgetFetch(code,"2014")
+	print("Done!")
